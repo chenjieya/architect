90b64044c7065a4f1cf3d9799d82b2184edb7309
Alvis <alvis.goodday@gmail.com>
2025-08-25 15:55:09 +0800

    rollup插件

---
+++ rollup/rollup-plugins/plugins/rollup-plugin-virtual.js
@@ -0,0 +1,8 @@
+export default function virtual(options = {}) {
+  return {
+    name: "virtual-example",
+    resolveId(id) {
+            
+    }
+  };
+}
---
+++ rollup/rollup-plugins/src/index.js
@@ -0,0 +1,9 @@
+import randomNumber from "./randomNumber";
+import deepClone from "./deepClone";
+import virtual from "rollup-plugin-virtual";
+
+// import("./sum.js").then((chunk) => {
+//   console.log(chunk.default(1, 2));
+// });
+
+export default { randomNumber, deepClone };
