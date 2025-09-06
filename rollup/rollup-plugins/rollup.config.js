ca21bdb78e2547f7bb1d95fd690cebc8407af117
chenjie <chenjie.goodday@gmail.com>
2025-09-06 16:11:40 +0800

    插件2 读取json

--- rollup/rollup-plugins/rollup.config.js
+++ rollup/rollup-plugins/rollup.config.js
@@ -1,16 +1,22 @@
 import { defineConfig } from 'rollup'
 import virtualExample from './plugins/rollup-plugin-virtual.js'
+import json from './plugins/rollup-plugin-json.js'
 
 const config = defineConfig({
 	input: 'src/index.js',
 	output: {
 		dir: 'dist',
 		format: 'esm',
+		sourcemap: true,
 		entryFileNames: 'input/[name]-[hash].js',
 		chunkFileNames: 'output/[name]-[hash].js'
 	},
 	plugins: [
-		virtualExample()
+		virtualExample(),
+		json({
+			include: './package.json',
+			exclude: 'node_modules/**'
+		})
 	]
 })
 
--- rollup/rollup-plugins/src/index.js
+++ rollup/rollup-plugins/src/index.js
@@ -4,6 +4,11 @@ import virtual from "virtual-module";
 
 console.log(virtual)
 
+// 读取json插件
+import pkg from '../package.json'
+console.log(pkg.name)
+console.log(pkg.version)
+
 // import("./sum.js").then((chunk) => {
 //   console.log(chunk.default(1, 2));
 // });
