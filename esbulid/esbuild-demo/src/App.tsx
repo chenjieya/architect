e7b67960404a333914adca4dd959ddbdf2a035c2
hp:jie.Chen <alvis.goodday@gmail.com>
2025-09-15 20:02:27 +0800

    【esbuild】

---
+++ esbulid/esbuild-demo/src/App.tsx
@@ -0,0 +1,4 @@
+import * as React from "react";
+import * as Server from "react-dom/server";
+let HelloWorldComp = () => <h1>hello world !!!</h1>;
+console.log(Server.renderToString(<HelloWorldComp />));
---
+++ esbulid/esbuild-demo/src/index.ts
@@ -0,0 +1,6 @@
+const sayHello = (name: string): string => {
+  console.log(`Hello, ${name}!`);
+  return `Hello, ${name}!`;
+};
+
+sayHello("Esbuild");
