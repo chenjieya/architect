7a72e5f1ab76d10b4c8c21577720445e3f5ff2db
mac:jie.Chen <chenjie.goodday@gmail.com>
2025-10-22 15:57:24 +0800

    【RN】LayoutAnimation动画

--- RN/movie/App.tsx
+++ RN/movie/App.tsx
@@ -1,81 +1,97 @@
-import { View, Text, StyleSheet, Pressable, Dimensions } from "react-native";
-import React, { PureComponent } from "react";
-import FreeDialog from "./components/freeDialog";
+import { useState } from "react";
+import {
+  View,
+  StyleSheet,
+  Pressable,
+  Text,
+  LayoutAnimation,
+  Platform,
+  UIManager
+} from "react-native";
 
-const { width } = Dimensions.get("window");
-
-interface IState {
-  isShow: boolean;
+if (
+  Platform.OS === "android" &&
+  UIManager.setLayoutAnimationEnabledExperimental
+) {
+  UIManager.setLayoutAnimationEnabledExperimental(true);
 }
 
-interface IProps {}
-
-export default class App extends PureComponent<IProps, IState> {
-  constructor(props: IProps) {
-    super(props);
-    this.state = {
-      isShow: false
-    };
+const customAnime = {
+  // 第一个动画
+  customSpring: {
+    duration: 10000,
+    create: {
+      springDamping: 0.3, // 弹跳动画阻尼系数
+      type: LayoutAnimation.Types.spring, // 动画类型
+      property: LayoutAnimation.Properties.scaleXY // 动画属性
+    },
+    update: {
+      springDamping: 0.6, // 弹跳动画阻尼系数
+      type: LayoutAnimation.Types.spring, // 动画类型
+      property: LayoutAnimation.Properties.scaleXY // 动画属性
+    }
+  },
+  // 第二个动画
+  customLinear: {
+    duration: 1000,
+    create: {
+      springDamping: 0.6, // 弹跳动画阻尼系数
+      type: LayoutAnimation.Types.linear, // 动画类型
+      property: LayoutAnimation.Properties.opacity // 动画属性
+    },
+    update: {
+      springDamping: 0.6, // 弹跳动画阻尼系数
+      type: LayoutAnimation.Types.linear, // 动画类型
+      property: LayoutAnimation.Properties.opacity // 动画属性
+    }
   }
-  // 点击按钮做的事情，打开谈框
-  pressHandler() {
-    this.setState({
-      isShow: !this.state.isShow
-    });
+};
+
+export default function App() {
+  const [width, setWidth] = useState(200);
+  const [height, setHeight] = useState(200);
+
+  function pressHandler() {
+    // 设置动画
+    LayoutAnimation.configureNext(customAnime.customLinear);
+
+    setWidth(width + 20);
+    setHeight(height + 20);
   }
 
-  renderFreeDialog() {
-    return (
-      <FreeDialog
-        isShow={this.state.isShow}
-        title="年底大促"
-        content="您的新年礼品，请查收！"
-        buttonContent="领取新年礼物"
-        imageSource={require("./assets/dialog_bg.png")}
-        closeDialog={() => {
-          this.setState({
-            isShow: false
-          });
+  return (
+    <View style={styles.container}>
+      <View
+        style={{
+          width,
+          height,
+          backgroundColor: "orange",
+          marginBottom: 30
         }}
-      />
-    );
-  }
-  render() {
-    return (
-      <View style={styles.container}>
-        <Pressable
-          style={styles.btnContainer}
-          onPress={this.pressHandler.bind(this)}
-        >
-          <Text style={styles.textStyle}>Press me</Text>
-        </Pressable>
-
-        {this.renderFreeDialog()}
-      </View>
-    );
-  }
+      ></View>
+      <Pressable
+        style={({ pressed }) => [
+          {
+            backgroundColor: pressed ? "yellow" : "red"
+          },
+          styles.button
+        ]}
+        onPress={pressHandler}
+      >
+        <Text>Press me</Text>
+      </Pressable>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center"
   },
-  btnContainer: {
-    marginTop: 15,
-    marginLeft: 10,
-    marginRight: 10,
-    backgroundColor: "#EE7942",
-    height: 38,
-    width: width - 100,
-    borderRadius: 5,
-    justifyContent: "center",
-    alignItems: "center"
-  },
-  textStyle: {
-    color: "#ffffff",
-    fontSize: 18
+  button: {
+    padding: 10,
+    borderRadius: 5
   }
 });
