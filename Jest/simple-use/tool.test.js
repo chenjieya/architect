cfee5c5679f0099674dbb5aa44f7798504199b00
hp:jie.Chen <alvis.goodday@gmail.com>
2025-11-09 22:07:05 +0800

    【Jest】 Jest基本入门

---
+++ Jest/simple-use/tool.test.js
@@ -0,0 +1,39 @@
+
+const { sum, sub, mul, div } = require("./tool");
+
+
+// test("加法测试", () => {
+//   expect(sum(1, 2)).toBe(3)
+// })
+
+// test("减法测试", () => {
+//   expect(sub(1, 2)).toBe(-1)
+// })
+
+// test("乘法测试", () => {
+//   expect(mul(1, 2)).toBe(2)
+// })
+
+// test("除法测试", () => {
+//   expect(div(1, 2)).toBe(0.5)
+// })
+
+describe("加减法组合测试", () => {
+  test("加法测试", () => {
+    expect(sum(1, 2)).toBe(3)
+  })
+
+  test("减法测试", () => {
+    expect(sub(1, 2)).toBe(-1)
+  })
+})
+
+describe("乘除法组合测试", () => {
+  test("乘法测试", () => {
+    expect(mul(1, 2)).toBe(2)
+  })
+
+  test("除法测试", () => {
+    expect(div(1, 2)).toBe(0.5)
+  })
+})
