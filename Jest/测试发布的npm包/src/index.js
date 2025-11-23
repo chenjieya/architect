62cac20d3b4e5832f234b463eee5c5b9e8bfa37a
hp:jie.Chen <alvis.goodday@gmail.com>
2025-11-23 20:07:20 +0800

    【Jest】测试npm发布的包

---
+++ Jest/测试发布的npm包/src/index.js
@@ -0,0 +1,6 @@
+const { rang } = require("jstooldesign")
+
+
+console.log(rang(0, 5)); // [0, 1, 2, 3, 4]
+console.log(rang(1, 5)); // [1, 2, 3, 4]
+console.log(rang(0, 5, 2)); // [0, 2, 4]
---
+++ Jest/测试发布的npm包/src/index2.js
@@ -0,0 +1,3 @@
+import { rang } from '../node_modules/jstooldesign/dist/bundle.esm.js'
+
+console.log(rang(0, 5));
