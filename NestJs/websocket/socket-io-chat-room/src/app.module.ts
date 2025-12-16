8dc923dd67bf436d87f84fdbfdc61c72c761390f
mac:jie.Chen <chenjie.goodday@gmail.com>
2025-12-16 17:59:50 +0800

    【Nest】聊天室项目增加环境配置

--- NestJs/websocket/socket-io-chat-room/src/app.module.ts
+++ NestJs/websocket/socket-io-chat-room/src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env.production', '.env'],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory(config: ConfigService) {
+        return {
+          type: 'mysql',
+          host: config.get('DB_HOST'),
+          port: config.get('DB_PORT'),
+          database: config.get('DB_DATABASE'),
+          name: config.get('DB_NAME'),
+          password: config.get('DB_PWD'),
+          entities: [],
+          synchronize: config.get('DB_CREATE') === 'true',
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
