0f36072b4aad0835670ebfbdcff3c6dc034eb0f3
hp:jie.Chen <alvis.goodday@gmail.com>
2026-01-17 16:31:38 +0800

    【Vite】代码分割

---
+++ Vite/vite-vue-code-splitting/vite.config.ts
@@ -0,0 +1,64 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { resolve } from "node:path";
+import ElementPlus from "unplugin-element-plus/vite";
+// import { chunkSplitPlugin } from "vite-plugin-chunk-split";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    ElementPlus({ format: "esm" })
+    // chunkSplitPlugin({
+    //   customSplitting: {
+    //     "vue-vender": ["vue", "vue-router"],
+    //     "element-plus": ["element-plus"],
+    //     "lodash-es": ["lodash-es"],
+    //     "components-util": [/src\/components/]
+    //   }
+    // })
+  ],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src")
+    }
+  },
+  build: {
+    minify: false,
+    //可以配置是否关闭css code split, 当组件启用代码分割的时候，css会自动帮我们进行代码分割，创建一个同名的css文件。
+    cssCodeSplit: false,
+
+    // 自动注入一个模块与加载器的polyfill
+    modulePreload: {
+      polyfill: true
+    },
+
+    rollupOptions: {
+      output: {
+        // chunkFileNames: "assets/[name].[hash].js",
+        // entryFileNames: "assets/[name].[hash].js",
+        // assetFileNames: "assets/[ext]/[name].[hash].[ext]"
+        // 手动配置打包分割
+        manualChunks: {
+          // 三方的vue相关的
+          vender: ["vue", "vue-router"],
+          "element-plus": ["element-plus"],
+          // lodash的分割
+          "lodash-es": ["lodash-es"]
+        }
+        // 函数式配置，但是简单配置分割可能会引起模块循环引用的问题
+        // manualChunks(id) {
+        //   if(id.includes('node_modules/vue')) {
+        //     return 'vue-vender'
+        //   }
+        //   if(id.includes('element-plus')) {
+        //     return 'element-plus'
+        //   }
+        //   if(id.includes('lodash-es')) {
+        //     return 'lodash-es'
+        //   }
+        // }
+      }
+    }
+  }
+});
