af4034d1673cd7ca282fa7b743e998d3caa593ae
mac:jie.Chen <chenjie.goodday@gmail.com>
2026-01-19 16:28:41 +0800

    【CLI】删除模版代码

--- 自定义脚手架/alvis-cli/asd/src/api/modules/user.ts
+++
@@ -1,7 +0,0 @@
-import { User,ResPage } from "@/api/interface";
-import request from "@/api/request";
-
-// 获取用户列表
-export const getUserList = (params?: User.ReqUserParams) => {
-  return request.get<ResPage<User.UserList>>(`/users/list`, params || {page:1,limit:10});
-};
